03aa5dd4f164956d07dcf9a9bae56a4e5bf34f7c
dankomanich <84127652+dankomanich@users.noreply.github.com>
2021-11-22 15:20:13 +0300

    Add files via upload

---
+++ build-tps_sim-Desktop-Debug/moc_device.cpp
@@ -0,0 +1,337 @@
+/****************************************************************************
+** Meta object code from reading C++ file 'device.h'
+**
+** Created by: The Qt Meta Object Compiler version 63 (Qt 4.8.7)
+**
+** WARNING! All changes made in this file will be lost!
+*****************************************************************************/
+
+#include "../tps-sim/device.h"
+#if !defined(Q_MOC_OUTPUT_REVISION)
+#error "The header file 'device.h' doesn't include <QObject>."
+#elif Q_MOC_OUTPUT_REVISION != 63
+#error "This file was generated using the moc from 4.8.7. It"
+#error "cannot be used with the include files from this version of Qt."
+#error "(The moc has changed too much.)"
+#endif
+
+QT_BEGIN_MOC_NAMESPACE
+static const uint qt_meta_data_Modules[] = {
+
+ // content:
+       6,       // revision
+       0,       // classname
+       0,    0, // classinfo
+       1,   14, // methods
+       0,    0, // properties
+       0,    0, // enums/sets
+       0,    0, // constructors
+       0,       // flags
+       0,       // signalCount
+
+ // slots: signature, parameters, type, tag, flags
+       9,    8,    8,    8, 0x0a,
+
+       0        // eod
+};
+
+static const char qt_meta_stringdata_Modules[] = {
+    "Modules\0\0updateWords()\0"
+};
+
+void Modules::qt_static_metacall(QObject *_o, QMetaObject::Call _c, int _id, void **_a)
+{
+    if (_c == QMetaObject::InvokeMetaMethod) {
+        Q_ASSERT(staticMetaObject.cast(_o));
+        Modules *_t = static_cast<Modules *>(_o);
+        switch (_id) {
+        case 0: _t->updateWords(); break;
+        default: ;
+        }
+    }
+    Q_UNUSED(_a);
+}
+
+const QMetaObjectExtraData Modules::staticMetaObjectExtraData = {
+    0,  qt_static_metacall 
+};
+
+const QMetaObject Modules::staticMetaObject = {
+    { &Device::staticMetaObject, qt_meta_stringdata_Modules,
+      qt_meta_data_Modules, &staticMetaObjectExtraData }
+};
+
+#ifdef Q_NO_DATA_RELOCATION
+const QMetaObject &Modules::getStaticMetaObject() { return staticMetaObject; }
+#endif //Q_NO_DATA_RELOCATION
+
+const QMetaObject *Modules::metaObject() const
+{
+    return QObject::d_ptr->metaObject ? QObject::d_ptr->metaObject : &staticMetaObject;
+}
+
+void *Modules::qt_metacast(const char *_clname)
+{
+    if (!_clname) return 0;
+    if (!strcmp(_clname, qt_meta_stringdata_Modules))
+        return static_cast<void*>(const_cast< Modules*>(this));
+    return Device::qt_metacast(_clname);
+}
+
+int Modules::qt_metacall(QMetaObject::Call _c, int _id, void **_a)
+{
+    _id = Device::qt_metacall(_c, _id, _a);
+    if (_id < 0)
+        return _id;
+    if (_c == QMetaObject::InvokeMetaMethod) {
+        if (_id < 1)
+            qt_static_metacall(this, _c, _id, _a);
+        _id -= 1;
+    }
+    return _id;
+}
+static const uint qt_meta_data_FunctionalDevice[] = {
+
+ // content:
+       6,       // revision
+       0,       // classname
+       0,    0, // classinfo
+       1,   14, // methods
+       0,    0, // properties
+       0,    0, // enums/sets
+       0,    0, // constructors
+       0,       // flags
+       0,       // signalCount
+
+ // slots: signature, parameters, type, tag, flags
+      18,   17,   17,   17, 0x08,
+
+       0        // eod
+};
+
+static const char qt_meta_stringdata_FunctionalDevice[] = {
+    "FunctionalDevice\0\0updateWords()\0"
+};
+
+void FunctionalDevice::qt_static_metacall(QObject *_o, QMetaObject::Call _c, int _id, void **_a)
+{
+    if (_c == QMetaObject::InvokeMetaMethod) {
+        Q_ASSERT(staticMetaObject.cast(_o));
+        FunctionalDevice *_t = static_cast<FunctionalDevice *>(_o);
+        switch (_id) {
+        case 0: _t->updateWords(); break;
+        default: ;
+        }
+    }
+    Q_UNUSED(_a);
+}
+
+const QMetaObjectExtraData FunctionalDevice::staticMetaObjectExtraData = {
+    0,  qt_static_metacall 
+};
+
+const QMetaObject FunctionalDevice::staticMetaObject = {
+    { &QWidget::staticMetaObject, qt_meta_stringdata_FunctionalDevice,
+      qt_meta_data_FunctionalDevice, &staticMetaObjectExtraData }
+};
+
+#ifdef Q_NO_DATA_RELOCATION
+const QMetaObject &FunctionalDevice::getStaticMetaObject() { return staticMetaObject; }
+#endif //Q_NO_DATA_RELOCATION
+
+const QMetaObject *FunctionalDevice::metaObject() const
+{
+    return QObject::d_ptr->metaObject ? QObject::d_ptr->metaObject : &staticMetaObject;
+}
+
+void *FunctionalDevice::qt_metacast(const char *_clname)
+{
+    if (!_clname) return 0;
+    if (!strcmp(_clname, qt_meta_stringdata_FunctionalDevice))
+        return static_cast<void*>(const_cast< FunctionalDevice*>(this));
+    return QWidget::qt_metacast(_clname);
+}
+
+int FunctionalDevice::qt_metacall(QMetaObject::Call _c, int _id, void **_a)
+{
+    _id = QWidget::qt_metacall(_c, _id, _a);
+    if (_id < 0)
+        return _id;
+    if (_c == QMetaObject::InvokeMetaMethod) {
+        if (_id < 1)
+            qt_static_metacall(this, _c, _id, _a);
+        _id -= 1;
+    }
+    return _id;
+}
+static const uint qt_meta_data_A2[] = {
+
+ // content:
+       6,       // revision
+       0,       // classname
+       0,    0, // classinfo
+       1,   14, // methods
+       0,    0, // properties
+       0,    0, // enums/sets
+       0,    0, // constructors
+       0,       // flags
+       0,       // signalCount
+
+ // slots: signature, parameters, type, tag, flags
+       4,    3,    3,    3, 0x0a,
+
+       0        // eod
+};
+
+static const char qt_meta_stringdata_A2[] = {
+    "A2\0\0updateWords()\0"
+};
+
+void A2::qt_static_metacall(QObject *_o, QMetaObject::Call _c, int _id, void **_a)
+{
+    if (_c == QMetaObject::InvokeMetaMethod) {
+        Q_ASSERT(staticMetaObject.cast(_o));
+        A2 *_t = static_cast<A2 *>(_o);
+        switch (_id) {
+        case 0: _t->updateWords(); break;
+        default: ;
+        }
+    }
+    Q_UNUSED(_a);
+}
+
+const QMetaObjectExtraData A2::staticMetaObjectExtraData = {
+    0,  qt_static_metacall 
+};
+
+const QMetaObject A2::staticMetaObject = {
+    { &Device::staticMetaObject, qt_meta_stringdata_A2,
+      qt_meta_data_A2, &staticMetaObjectExtraData }
+};
+
+#ifdef Q_NO_DATA_RELOCATION
+const QMetaObject &A2::getStaticMetaObject() { return staticMetaObject; }
+#endif //Q_NO_DATA_RELOCATION
+
+const QMetaObject *A2::metaObject() const
+{
+    return QObject::d_ptr->metaObject ? QObject::d_ptr->metaObject : &staticMetaObject;
+}
+
+void *A2::qt_metacast(const char *_clname)
+{
+    if (!_clname) return 0;
+    if (!strcmp(_clname, qt_meta_stringdata_A2))
+        return static_cast<void*>(const_cast< A2*>(this));
+    return Device::qt_metacast(_clname);
+}
+
+int A2::qt_metacall(QMetaObject::Call _c, int _id, void **_a)
+{
+    _id = Device::qt_metacall(_c, _id, _a);
+    if (_id < 0)
+        return _id;
+    if (_c == QMetaObject::InvokeMetaMethod) {
+        if (_id < 1)
+            qt_static_metacall(this, _c, _id, _a);
+        _id -= 1;
+    }
+    return _id;
+}
+static const uint qt_meta_data_PP4[] = {
+
+ // content:
+       6,       // revision
+       0,       // classname
+       0,    0, // classinfo
+      10,   14, // methods
+       0,    0, // properties
+       0,    0, // enums/sets
+       0,    0, // constructors
+       0,       // flags
+       0,       // signalCount
+
+ // slots: signature, parameters, type, tag, flags
+       5,    4,    4,    4, 0x08,
+      25,   19,    4,    4, 0x08,
+      46,   19,    4,    4, 0x08,
+      68,   19,    4,    4, 0x08,
+      93,   19,    4,    4, 0x08,
+     118,   19,    4,    4, 0x08,
+     143,   19,    4,    4, 0x08,
+     165,   19,    4,    4, 0x08,
+     187,   19,    4,    4, 0x08,
+     212,   19,    4,    4, 0x08,
+
+       0        // eod
+};
+
+static const char qt_meta_stringdata_PP4[] = {
+    "PP4\0\0updateWords()\0check\0activateAllOut(bool)\0"
+    "activateAllInOn(bool)\0activateAllInReady(bool)\0"
+    "activateAllinFail1(bool)\0"
+    "activateAllInFail2(bool)\0activateAllNSD1(bool)\0"
+    "activateAllNSD2(bool)\0activateAllInLocal(bool)\0"
+    "updateWorking(bool)\0"
+};
+
+void PP4::qt_static_metacall(QObject *_o, QMetaObject::Call _c, int _id, void **_a)
+{
+    if (_c == QMetaObject::InvokeMetaMethod) {
+        Q_ASSERT(staticMetaObject.cast(_o));
+        PP4 *_t = static_cast<PP4 *>(_o);
+        switch (_id) {
+        case 0: _t->updateWords(); break;
+        case 1: _t->activateAllOut((*reinterpret_cast< bool(*)>(_a[1]))); break;
+        case 2: _t->activateAllInOn((*reinterpret_cast< bool(*)>(_a[1]))); break;
+        case 3: _t->activateAllInReady((*reinterpret_cast< bool(*)>(_a[1]))); break;
+        case 4: _t->activateAllinFail1((*reinterpret_cast< bool(*)>(_a[1]))); break;
+        case 5: _t->activateAllInFail2((*reinterpret_cast< bool(*)>(_a[1]))); break;
+        case 6: _t->activateAllNSD1((*reinterpret_cast< bool(*)>(_a[1]))); break;
+        case 7: _t->activateAllNSD2((*reinterpret_cast< bool(*)>(_a[1]))); break;
+        case 8: _t->activateAllInLocal((*reinterpret_cast< bool(*)>(_a[1]))); break;
+        case 9: _t->updateWorking((*reinterpret_cast< bool(*)>(_a[1]))); break;
+        default: ;
+        }
+    }
+}
+
+const QMetaObjectExtraData PP4::staticMetaObjectExtraData = {
+    0,  qt_static_metacall 
+};
+
+const QMetaObject PP4::staticMetaObject = {
+    { &Device::staticMetaObject, qt_meta_stringdata_PP4,
+      qt_meta_data_PP4, &staticMetaObjectExtraData }
+};
+
+#ifdef Q_NO_DATA_RELOCATION
+const QMetaObject &PP4::getStaticMetaObject() { return staticMetaObject; }
+#endif //Q_NO_DATA_RELOCATION
+
+const QMetaObject *PP4::metaObject() const
+{
+    return QObject::d_ptr->metaObject ? QObject::d_ptr->metaObject : &staticMetaObject;
+}
+
+void *PP4::qt_metacast(const char *_clname)
+{
+    if (!_clname) return 0;
+    if (!strcmp(_clname, qt_meta_stringdata_PP4))
+        return static_cast<void*>(const_cast< PP4*>(this));
+    return Device::qt_metacast(_clname);
+}
+
+int PP4::qt_metacall(QMetaObject::Call _c, int _id, void **_a)
+{
+    _id = Device::qt_metacall(_c, _id, _a);
+    if (_id < 0)
+        return _id;
+    if (_c == QMetaObject::InvokeMetaMethod) {
+        if (_id < 10)
+            qt_static_metacall(this, _c, _id, _a);
+        _id -= 10;
+    }
+    return _id;
+}
+QT_END_MOC_NAMESPACE
